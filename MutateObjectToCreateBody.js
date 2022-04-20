965893ca3c58c4f83c13c6cf054ae6a9c3204859
Abdullah Ansari <aa9272137@gmail.com>
2022-04-20 12:53:36 +0500

    Create MutateObjectToCreateBody.js

---
+++ MutateObjectToCreateBody.js
@@ -0,0 +1,390 @@
+const obj = {
+    "IDPrefix": {
+        "type": "dropdown",
+        "value": "1",
+        "options": "prefixes"
+    },
+    "FirstName": {
+        "type": "text",
+        "value": "Abdullah"
+    },
+    "LastName": {
+        "type": "text",
+        "value": "Ansari"
+    },
+    "Nickname": {
+        "type": "text",
+        "value": null
+    },
+    "IDGender": {
+        "type": "dropdown",
+        "value": null,
+        "options": "genders"
+    },
+    "Title": {
+        "type": "text",
+        "value": null
+    },
+    "Careof": {
+        "type": "text",
+        "value": null
+    },
+    "EmailAddress": {
+        "type": "text",
+        "value": null
+    },
+    "Line1": {
+        "type": "text",
+        "value": "Magical Digits, Office # 205, Gulshan Trade Center, Block 5 Gulshan-e-Iqbal, Karachi, 75300"
+    },
+    "Line2": {
+        "type": "text",
+        "value": "205"
+    },
+    "IDCountry": {
+        "type": "country",
+        "value": "1"
+    },
+    "IDState": {
+        "type": "state",
+        "value": "4"
+    },
+    "IDCity": {
+        "type": "city",
+        "value": "7570"
+    },
+    "Postal": {
+        "type": "text",
+        "value": "75300"
+    },
+    "Website": {
+        "type": "text",
+        "value": null
+    },
+    "PhoneWork": {
+        "type": "text",
+        "value": "090078601"
+    },
+    "PhoneWorkExtension": {
+        "type": "text",
+        "value": "1"
+    },
+    "PhoneHome": {
+        "type": "text",
+        "value": "1"
+    },
+    "PhoneHomeExtension": {
+        "type": "text",
+        "value": "1"
+    },
+    "PhoneMobile": {
+        "type": "text",
+        "value": "1"
+    },
+    "PhoneMobileExtension": {
+        "type": "text",
+        "value": "1"
+    },
+    "PhoneFax": {
+        "type": "text",
+        "value": "1"
+    },
+    "PhoneFaxExtension": {
+        "type": "text",
+        "value": "1"
+    },
+    "IMaddress": {
+        "type": "text",
+        "value": "1"
+    },
+    "IMvendor": {
+        "type": "dropdown",
+        "value": "2562",
+        "options": "vendors"
+    },
+    "Manager": {
+        "type": "dropdown",
+        "value": "383",
+        "options": "managers"
+    },
+    "SourceID": {
+        "type": "dropdown",
+        "value": "16",
+        "options": "sources"
+    },
+    "MediaID": {
+        "type": "dropdown",
+        "value": "12",
+        "options": "media"
+    },
+    "Originator": {
+        "type": "dropdown",
+        "value": null,
+        "options": "originators"
+    },
+    "Tagged": {
+        "type": "text",
+        "value": null
+    },
+    "CommunicationControls": {
+        "type": "label",
+        "value": "Communication Controls"
+    },
+    "ReturnedMail": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DoNotCall": {
+        "type": "checkbox",
+        "value": null
+    },
+    "DoNotMail": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "Deceased": {
+        "type": "checkbox",
+        "value": null
+    },
+    "EmailControls": {
+        "type": "label",
+        "value": "Email Controls"
+    },
+    "DoNotEmail": {
+        "type": "checkbox",
+        "value": null
+    },
+    "Unsubscribe": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "Bounced": {
+        "type": "checkbox",
+        "value": null
+    },
+    "EmailVerificationReady": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "NotVerified": {
+        "type": "checkbox",
+        "value": null
+    },
+    "Active": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "Lockedd": {
+        "type": "checkbox",
+        "value": null
+    },
+    "Profile": {
+        "type": "textarea",
+        "value": "<h1>Abdullah</h1>"
+    },
+    "NotesPersonal": {
+        "type": "textarea",
+        "value": "no comments"
+    },
+    "NotesPrivate": {
+        "type": "textarea",
+        "value": null
+    },
+    "Samantha": {
+        "type": "label",
+        "value": "Samantha"
+    },
+    "SourceGM": {
+        "type": "text",
+        "value": "a"
+    },
+    "Referror": {
+        "type": "text",
+        "value": "a"
+    },
+    "Assistant": {
+        "type": "text",
+        "value": "a"
+    },
+    "Contact": {
+        "type": "text",
+        "value": "a"
+    },
+    "Goldmine": {
+        "type": "label",
+        "value": "Goldmine"
+    },
+    "GoldMineSource": {
+        "type": "text",
+        "value": "a"
+    },
+    "GoldMineReferror": {
+        "type": "text",
+        "value": "a"
+    },
+    "GoldMineAssistant": {
+        "type": "text",
+        "value": "a"
+    },
+    "RentalFieldsImportedFromGoldmine": {
+        "type": "label",
+        "value": "Rental fields imported from Goldmine"
+    },
+    "RentalName": {
+        "type": "text",
+        "value": "a"
+    },
+    "RentalAddress": {
+        "type": "text",
+        "value": "Magical Digits, Office # 205, Gulshan Trade Center, Block 5 Gulshan-e-Iqbal, Karachi, 75300"
+    },
+    "RentalCity": {
+        "type": "text",
+        "value": "Chiniot"
+    },
+    "RentalState": {
+        "type": "text",
+        "value": "Punjab"
+    },
+    "RentalComplex": {
+        "type": "text",
+        "value": "a"
+    },
+    "RentalNeighbor": {
+        "type": "text",
+        "value": "a"
+    },
+    "RentalCountry": {
+        "type": "text",
+        "value": "Pakistan"
+    },
+    "ReferenceFieldsImportedFromGoldmine": {
+        "type": "label",
+        "value": "Reference fields imported from Goldmine"
+    },
+    "GoldMineTagged": {
+        "type": "text",
+        "value": "a"
+    },
+    "GoldmineParcel": {
+        "type": "text",
+        "value": "a"
+    },
+    "GoldmineZone": {
+        "type": "text",
+        "value": "a"
+    },
+    "GoldMineAirbnb": {
+        "type": "text",
+        "value": "a"
+    },
+    "GoldMineFlipKey": {
+        "type": "text",
+        "value": "a"
+    },
+    "GoldMineHomeAway": {
+        "type": "text",
+        "value": "a"
+    },
+    "GoldMineVRBO": {
+        "type": "text",
+        "value": "a"
+    },
+    "Displays": {
+        "type": "label",
+        "value": "Displays"
+    },
+    "DisplaysName": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysAddresses": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysEddresses": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysPhones": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysSocials": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysCareof": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysTitle": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysSpouse": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysChildren": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysStatus": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "Status": {
+        "type": "text",
+        "value": null
+    },
+    "DisplaysCity": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysState": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysCountry": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysPostal": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysPhoneWork": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysPhoneHome": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysPhoneMobile": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysPhoneTollFree": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysPhoneFax": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysProfile": {
+        "type": "checkbox",
+        "value": "-1"
+    },
+    "DisplaysWebsites": {
+        "type": "checkbox",
+        "value": "-1"
+    }
+}
+let newobj = {}
+Object.keys(obj).forEach(function(key) {
+    newobj[key] = obj[key].value
+})
+
